9f6f0f835eb9d450e081f2d2711f202b4e0b5698
k-gn <rlarbska97@gmail.com>
2025-03-20 20:58:11 +0900

    3. 객체지향 패러다임 - 2

--- src/main/java/cleancode/minesweeper/tobe/Cell.java
+++ src/main/java/cleancode/minesweeper/tobe/Cell.java
@@ -9,14 +9,23 @@ public class Cell {
 	public static final String UNCHECKED_SIGN = "□";
 	public static final String EMPTY_SIGN = "■";
 
-	private boolean isLandMine;
-	private boolean isFlagged;
 	private boolean isOpened;
+	private boolean isFlagged;
+	private boolean isLandMine;
 	private int nearByLandMineCount;
 
 	/*
 		속성 : 근처 지뢰 숫자, 지뢰 여부
 		상태 : 깃발 유무, 열림/닫힘, 사용자 확인
+
+	    * 도메인 지식은 발견하는 것이다.
+		* 이전엔 몰랐던 도메인 지식 습득!
+		  - 열렸다/닫혔다 개념은 사용자가 체크했다는 개념과 다르다.
+		  - 게임 종료 조건 : 모든 셀이 열려있거나, 닫혀있지만 깃발로 확인된 경우
+		  - 단순히 열렸다/닫혔다로 게임을 종료 시킬 수 있는게 아닌 사용자의 체크 유무로 종료시킬 수 있다.
+		    - 기존엔 BOARD 배열에서 sign 을 갈아끼우는 형태
+		      -> cell 이라는 정보를 담을 객체가 생김
+		      -> 사용자의 행위에 따라 cell 의 상태를 변화시키는 방향으로 변경
 	 */
 
 	public Cell(
@@ -44,10 +53,6 @@ public class Cell {
 		return of(0, false, false, false);
 	}
 
-	public void turnOnLandMine() {
-		this.isLandMine = true;
-	}
-
 	public void flag() {
 		this.isFlagged = true;
 	}
@@ -56,8 +61,8 @@ public class Cell {
 		this.isOpened = true;
 	}
 
-	public boolean isChecked() {
-		return isFlagged || isOpened;
+	public void turnOnLandMine() {
+		this.isLandMine = true;
 	}
 
 	public boolean isLandMine() {
@@ -66,6 +71,10 @@ public class Cell {
 
 	public boolean isOpened() {
 		return isOpened;
+	}
+
+	public boolean isChecked() {
+		return isFlagged || isOpened;
 	}
 
 	public boolean hasLandMineCount() {
